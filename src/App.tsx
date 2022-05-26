2e63fe05fe5773067ca6e02ee32f307533fd7247
theHasanas <haltabtabai@bookpeep.com>
2022-05-26 23:21:04 +0300

    Add basic scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-const slots = ["12:00", "12:15", "12:30", "12:45", "13:00"];
+import { Box, Button, Drawer, Typography } from "@mui/material";
+
+import { useState } from "react";
+
+// Assume data arrives like this
+
+// {
+//   morning: ["10:00 am", "10:15 am", "10:30 am", "10:45 am", "11:00 am"],
+//   afternoon: ["12:00 pm", "12:15 pm", "12:30 pm", "12:45 pm", "01:00 pm"],
+//   evening: ["08:00 pm", "08:15 pm", "08:30 pm", "08:45 pm", "09:00 pm"],
+// };
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="App">
-      Assume data arrives as strings in this form: {JSON.stringify(slots)}
-    </div>
+    <Box
+      width="100vw"
+      height="100vh"
+      bgcolor="#222222"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      gap={4}
+    >
+      <Typography style={{ fontSize: 30, color: "white" }}>
+        Build A slot Picker
+      </Typography>
+
+      <Button onClick={() => setIsModalOpen(!isModalOpen)} variant="contained">
+        Open Modal
+      </Button>
+
+      <Drawer
+        anchor="bottom"
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(!isModalOpen)}
+        PaperProps={{
+          sx: { padding: 4 },
+        }}
+      >
+        <Typography fontWeight={500}>Slot picker should be here.</Typography>
+      </Drawer>
+    </Box>
   );
 }
 
